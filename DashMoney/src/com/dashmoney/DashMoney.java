0d1b3c752dd0236f33521927720497080b6e2b0b
Freya <KvinneKraft@protonmail.com>
2020-01-31 01:04:04 +0200

    -Java

---
+++ DashMoney/src/com/dashmoney/DashMoney.java
@@ -0,0 +1,25 @@
+
+// Author: Dashie
+// Version: 1.0
+
+package com.dashmoney;
+
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+
+public class DashMoney extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        
+    };
+    
+    @Override
+    public void onDisable()
+    {
+        
+    };
+};
