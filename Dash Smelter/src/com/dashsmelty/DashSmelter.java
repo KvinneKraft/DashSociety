83037cac9cce6aef23aa904a49b64cff13c71b93
Freya <KvinneKraft@protonmail.com>
2020-01-31 05:01:56 +0200

    -Java

---
+++ Dash Smelter/src/com/dashsmelty/DashSmelter.java
@@ -0,0 +1,20 @@
+
+package com.dashsmelty;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DashSmelter extends JavaPlugin
+{
+    
+    
+    @Override
+    public void onEnable()
+    {
+    };
+    
+    @Override
+    public void onDisable()
+    {
+        
+    };
+};
