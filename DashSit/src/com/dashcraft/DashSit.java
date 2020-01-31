5bd6386c238bf808e82b4759c568d7a50c81adbb
Freya <KvinneKraft@protonmail.com>
2020-01-31 08:16:23 +0200

    -Java

--- DashSit/src/com/dashcraft/DashSit.java
+++ DashSit/src/com/dashcraft/DashSit.java
@@ -1,20 +1,35 @@
 
+// Author: Dashie
+// Version: 1.0
+
 package com.dashcraft;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DashSit extends JavaPlugin
 {
+    public static JavaPlugin plugin;
+    public static FileConfiguration config;
+    
     @Override
     public void onEnable()
     {
-        // To-Do:
-        //
-        // - Initialization Messages.
-        // - Link Moon Library.
-        // - Add Player Passenger to Invisible Object.
-        // - Remove Invisible Object when Player Gets off.
-        // - Register /sit Command
+        Luna.print("Initializing the Sit Plugin ....");
+        
+        saveDefaultConfig();
+        
+        config = getConfig();
+        plugin = this;
+        
+        getCommand("sit").setExecutor(new CommandHandler());
+        getServer().getPluginManager().registerEvents(new EventsHandler(), plugin);
+        
+        Luna.print("Author: Dashie");
+        Luna.print("Plugin Name: DashSit);");
+        Luna.print("Version: 1.0");
+        
+        Luna.print("The Sit Plugin is now running in the Background!");
     };
     
     @Override
---
+++ DashSit/src/com/dashcraft/EventsHandler.java
@@ -0,0 +1,60 @@
+
+package com.dashcraft;
+
+import org.bukkit.Location;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.spigotmc.event.entity.EntityDismountEvent;
+
+public class EventsHandler implements Listener
+{
+    FileConfiguration config = Luna.getGlobalConfig();
+    
+    String standMessage = Luna.transStr(config.getString("stand-message"));
+    
+    @EventHandler
+    public void onEntityDismount(EntityDismountEvent e)
+    {
+        if(!(e.getEntity() instanceof Player))
+            return;
+        
+        else if(!(e.getDismounted() instanceof ArmorStand))
+            return;
+                    
+        if(e.getDismounted().getMetadata("chair") != null)
+        {
+            e.getDismounted().remove();
+            
+            Player p = (Player) e.getEntity();
+            
+            p.sendMessage(standMessage);
+        };
+    };
+    
+    Luna luna = new Luna();
+    
+    @EventHandler
+    public void onInteract(PlayerInteractEvent e)
+    {
+        if(e.getClickedBlock() == null)
+            return;
+        
+        if(e.getAction() == Action.RIGHT_CLICK_BLOCK)
+        {
+            if(e.getClickedBlock().getType().toString().toLowerCase().contains("stairs"))
+            {
+                
+                Location location = e.getClickedBlock().getLocation();
+                
+                location.add(0.5,-1.2,0.5);
+                
+                luna.SitDown(e.getPlayer(), location);
+            };
+        };
+    };
+};
