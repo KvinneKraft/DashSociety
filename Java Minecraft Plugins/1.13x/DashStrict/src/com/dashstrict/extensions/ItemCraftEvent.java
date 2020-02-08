05c4a14dbb1524f063ae61d38c99dacbbe694a72
Freya <KvinneKraft@protonmail.com>
2020-02-08 22:58:04 +0200

    -Java

---
+++ Java Minecraft Plugins/1.13x/DashStrict/src/com/dashstrict/extensions/ItemCraftEvent.java
@@ -0,0 +1,12 @@
+
+package com.dashstrict.extensions;
+
+import org.bukkit.event.inventory.CraftItemEvent;
+
+public class ItemCraftEvent
+{
+    public static void onCraft(CraftItemEvent e)
+    {
+        
+    };
+};
