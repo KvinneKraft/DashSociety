ffb3f240c1d2e65bbfac0393a2c4e2a7114abc1a
Dashie <mystical_cxder_Dashie@protonmail.com>
2020-02-27 09:02:30 +0100

    -Code

---
+++ Fluffy Luna/Interactive Shells/Dash Society/DashSociety.lua
@@ -0,0 +1,54 @@
+
+-- Dash Society is a client for any operating system
+-- that has LUA 5.2 installed.
+
+-- Author: Dashie
+-- Version: 1.0
+
+
+function get_directory()
+    local chr = os.tmpname():sub(1, 1)
+
+    if (chr == "/")
+        then
+            chr = "/[^/]*$"
+        end
+    else
+        chr = "\\^[\\]*$"
+    end
+
+    return arg[0]:sub(1, arg[0]:find(chr) .. ("DashSociety.lua" or ''))
+end
+
+function sys(message)
+    io.write("[info]: " .. message .. " \n");
+end
+
+function pr(message)
+    io.write(message)
+end
+
+function execute_command(command)
+    if (command == "shell")
+        then
+            while (true) do
+                sys("You are now in the shell of the Dash.")
+                sys("Type \'!bye\' at any time in order to go back.\n")
+
+                pr("$[/Dash Shell/]: ")
+            end
+        end
+    else if (command == "cls")
+        then
+            os.execute("clear")
+        end
+    else
+        sys("command is unknown, use ? for help")
+    end
+end
+
+function main()
+    while (true) do
+
+    end
+end
