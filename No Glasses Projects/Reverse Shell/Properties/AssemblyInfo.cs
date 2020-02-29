c07c60102958e059e867f7318be3748e0009f4a2
Freya <KvinneKraft@protonmail.com>
2020-02-29 18:45:08 +0200

    -Code

---
+++ No Glasses Projects/Reverse Shell/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Reverse Shell")]
+[assembly: AssemblyDescription("1.0")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Dashies Softwaries")]
+[assembly: AssemblyProduct("Reverse Shell")]
+[assembly: AssemblyCopyright("Copyright © KvinneKraft 2020")]
+[assembly: AssemblyTrademark("Dashies Softwaries")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("f99263c4-2a13-4cd0-b820-b7ccdecfa2c0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
