7dd893347af6c796c2a0e57a7d5a2cbeedbe41f8
Dashie <mystical_cxder_Dashie@protonmail.com>
2020-03-12 07:59:44 +0100

    -Code

---
+++ MineCrack/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+//
+// Author: Dashie
+// Version: 1.0
+//
+
+
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+
+namespace MineCraft
+{
+    public partial class KvinneKraft : Form
+    {
+	public KvinneKraft()
+	{
+
+	}
+    };
+
+
+    public static class Program
+    {
+	private static readonly KvinneKraft kvinnekraft = new KvinneKraft();
+
+	[STAThread]
+	public static void Main()
+	{
+	    Application.EnableVisualStyles();
+	    Application.SetCompatibleTextRenderingDefault(false);
+	    Application.Run(kvinnekraft);
+	}
+    }
+}
