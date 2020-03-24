d513f6251b8919145654ba5f77796eca9e6bead4
Freya <KvinneKraft@protonmail.com>
2020-03-24 11:24:59 +0200

    -Java

--- Java Minecraft Plugins/1.13x/DashHat/src/com/dashcraft/DashHat.java
+++ Java Minecraft Plugins/1.13x/DashHat/src/com/dashcraft/DashHat.java
@@ -7,16 +7,15 @@
 package com.dashcraft;
 
 
-import java.util.ArrayList;
-import java.util.List;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.potion.PotionEffectType;
 
 
 public class DashHat extends JavaPlugin implements CommandExecutor
@@ -43,6 +42,8 @@ public class DashHat extends JavaPlugin implements CommandExecutor
         config = (FileConfiguration) getConfig();
         plugin = (JavaPlugin) this;
         
+        load();
+        
         getCommand("dashhat").setExecutor(plugin);
         
         print("I am done, I am now running!");
@@ -53,10 +54,7 @@ public class DashHat extends JavaPlugin implements CommandExecutor
         print("Plugin has now been disabled.");
     };
     
-    private List<Player> players = new ArrayList<>();
-    
     private String admin_permission, hat_permission;
-    private int hat_cooldown;
     
     private void load()
     {
@@ -65,7 +63,6 @@ public class DashHat extends JavaPlugin implements CommandExecutor
         
         admin_permission = config.getString("properties.admin-permission");        
         hat_permission = config.getString("properties.hat-permission");
-        hat_cooldown = config.getInt("properties.hat-cooldown");
     };
     
     @Override public boolean onCommand(CommandSender s, Command c, String a, String[] as)
@@ -77,12 +74,30 @@ public class DashHat extends JavaPlugin implements CommandExecutor
         
         if (as.length >= 1 && as[0].equalsIgnoreCase("reload") && p.hasPermission(admin_permission))
         {
+            p.sendMessage(color("&e>>> &a&lReloading ...."));
             
+            load();
+            
+            p.sendMessage(color("&e>>> &a&lDone!"));
         }
             
         else if ((p.hasPermission(hat_permission) || p.hasPermission(admin_permission)))
         {
+            ItemStack item = p.getInventory().getItemInMainHand();
             
+            if(item == null || item.getType() == Material.AIR)
+            {
+                p.sendMessage(color("&cYou must be holding something besides air."));
+                return false;
+            };
+            
+            p.getInventory().setHelmet(item);
+            p.sendMessage("&aThere ye go, your custom head!");
+        }
+        
+        else
+        {
+            p.sendMessage(color("&cYe do not has the purrmissions do ye?"));
         };
         
         return true;
