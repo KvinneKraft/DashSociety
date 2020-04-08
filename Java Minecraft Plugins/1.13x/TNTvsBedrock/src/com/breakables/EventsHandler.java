deba4945daa9420ef55224200fc6b10c0edde06a
Freya <KvinneKraft@protonmail.com>
2020-04-09 00:38:35 +0300

    -Code

---
+++ Java Minecraft Plugins/1.13x/TNTvsBedrock/src/com/breakables/EventsHandler.java
@@ -0,0 +1,16 @@
+
+// Author: Dashie
+// Version: 1.0
+
+package com.breakables;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockExplodeEvent;
+
+public class EventsHandler implements Listener
+{
+    public void onExplode(BlockExplodeEvent e)
+    {
+        // e.getBlock();
+    };
+};
