89f1a5d1b1873dcff70c8ebb0e707c7204b22194
Freya <KvinneKraft@protonmail.com>
2020-04-08 15:53:38 +0300

    -Code

---
+++ No Glasses Projects/Dash IP Fluffer/DashFluff.cs
@@ -0,0 +1,28 @@
+﻿
+// Author: Dashie
+// Version: 1.0
+
+using System;
+using System.Windows.Forms;
+
+namespace Dash_IP_Fluffer
+{
+    static class Program
+    {
+	[STAThread]
+	static void Main()
+	{
+	    Application.EnableVisualStyles();
+	    Application.SetCompatibleTextRenderingDefault(false);
+	    Application.Run(new Interfuce());
+	}
+    };
+
+    public partial class Interfuce : Form
+    {
+	public Interfuce()
+	{
+
+	}
+    };
+};
