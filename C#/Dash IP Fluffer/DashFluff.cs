d3c601ac4b0da241f5747a96bbafefb44681a667
Freya <KvinneKraft@protonmail.com>
2020-04-11 03:23:01 +0300

    code

--- C#/Dash IP Fluffer/DashFluff.cs
+++ C#/Dash IP Fluffer/DashFluff.cs
@@ -1,9 +1,14 @@
 ﻿
+
 // Author: Dashie
 // Version: 1.0
 
+
 using System;
+using System.Text;
+using System.Drawing;
 using System.Windows.Forms;
+
 
 namespace Dash_IP_Fluffer
 {
@@ -20,9 +25,27 @@ namespace Dash_IP_Fluffer
 
     public partial class Interfuce : Form
     {
+	private void SetupLayout()
+	{
+	    BackColor = Color.FromArgb(24, 24, 24);
+
+	    FormBorderStyle = FormBorderStyle.None;
+	    StartPosition = FormStartPosition.CenterScreen;
+
+	    Icon = (Icon)Properties.Resources.icon;
+
+	    Size siz = new Size(350, 300);
+
+	    Size = siz;
+	    MinimumSize = siz;
+	    MaximumSize = siz;
+
+	    Add.InteractiveToolBar(this);
+	}
+
 	public Interfuce()
 	{
-
+	    SetupLayout();
 	}
     };
 };
