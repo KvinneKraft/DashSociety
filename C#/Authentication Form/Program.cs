11f80f5c11624b8c1f0cb862a3e8e07a9b826cd8
Freya <KvinneKraft@protonmail.com>
2020-04-30 22:50:32 +0300

    -Code

--- C#/Authentication Form/Program.cs
+++ C#/Authentication Form/Program.cs
@@ -8,9 +8,6 @@ namespace Authentication_Form
 {
     static class Program
     {
-	/// <summary>
-	/// The main entry point for the application.
-	/// </summary>
 	[STAThread]
 	static void Main()
 	{
--- Public Personal/KvinneKraft/DashConsilience/src/com/philosophy/Organisms.java
+++ Public Personal/KvinneKraft/DashConsilience/src/com/philosophy/Organisms.java
@@ -7,13 +7,21 @@ package com.philosophy;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Creeper;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Skeleton;
 import org.bukkit.entity.Stray;
+import org.bukkit.entity.Zombie;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.inventory.EntityEquipment;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
@@ -70,8 +78,54 @@ public class Organisms
         };
     };
     
+    final static ItemStack diamond_sword = new ItemStack(Material.DIAMOND_SWORD, 1);    
+    
+    final static ItemStack diamond_helmet = new ItemStack(Material.DIAMOND_HELMET, 1);
+    final static ItemStack diamond_chestplate = new ItemStack(Material.DIAMOND_CHESTPLATE, 1);
+    final static ItemStack diamond_leggings = new ItemStack(Material.DIAMOND_LEGGINGS, 1);
+    final static ItemStack diamond_boots = new ItemStack(Material.DIAMOND_BOOTS, 1);
+    
     public static void onEntitySpawn(final CreatureSpawnEvent e)
     {
+        final LivingEntity entity = (LivingEntity) e.getEntity();
         
+        if (entity instanceof Creeper)
+        {
+            if (rand.nextInt(100) < 25)
+            {
+                ((Creeper)entity).setPowered(true);
+            }
+        }
+        
+        else if (entity instanceof Skeleton)
+        {
+            if (rand.nextInt(100) < 5)
+            {
+                entity.getWorld().spawnEntity(e.getLocation(), EntityType.WITHER_SKELETON);
+            };
+        }
+        
+        else if (entity instanceof Zombie)
+        {   
+            if (rand.nextInt(100) < 95)
+            {
+                return;
+            };
+            
+            if (!diamond_sword.containsEnchantment(Enchantment.DAMAGE_ALL))
+            {
+                diamond_sword.addUnsafeEnchantment(Enchantment.FIRE_ASPECT, 10);
+                diamond_sword.addUnsafeEnchantment(Enchantment.DAMAGE_ALL, 10);
+            };
+            
+            final EntityEquipment equip = (EntityEquipment) entity.getEquipment();
+            
+            equip.setHelmet(diamond_helmet);
+            equip.setChestplate(diamond_chestplate);
+            equip.setLeggings(diamond_leggings);
+            equip.setBoots(diamond_boots);
+            
+            equip.setItemInMainHand(diamond_sword);
+        }
     };
 };
