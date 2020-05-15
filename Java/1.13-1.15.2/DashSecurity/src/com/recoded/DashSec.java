796cc939da0f1c44c021eed60ee3a4f6ffd2a45a
KvinneKraft <KvinneKraft@protonmail.com>
2020-05-15 19:30:24 +0200

    -Code

--- Java/1.13-1.15.2/DashSecurity/src/com/recoded/DashSec.java
+++ Java/1.13-1.15.2/DashSecurity/src/com/recoded/DashSec.java
@@ -2,12 +2,12 @@ package com.recoded;
 
 public class DashSec
 {
-    public static void print(final String line)
+    public void print(final String line)
     {
         System.out.println("(Dash Security): " + line);
     };
     
-    public static String color(final String line)
+    public String color(final String line)
     {
         return ChatColor.translateAlternateColorCodes('&', line);
     };
--- Java/1.13-1.15.2/DashSecurity/src/com/recoded/DashSecurity.java
+++ Java/1.13-1.15.2/DashSecurity/src/com/recoded/DashSecurity.java
@@ -5,13 +5,22 @@ package com.recoded;
 
 public class DashSecurity extends JavaPlugin
 {
+    private final DashSec sec = new DashSec();
+    
     @Override public void onEnable()
     {
+        sec.print("Plugin is being enabled ....");
         
+        
+        
+        sec.print("Plugin has been enabled!");
     };
+    
+    public static FileConfiguration config = (FileConfiguration) null;
+    public static JavaPlugin plugin = (JavaPlugin) this;
     
     @Override public void onDisable()
     {
-        
+        sec.print("Plugin has been disabled!");
     };
 };
