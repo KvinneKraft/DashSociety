2d668fb578ea5e7c25b575c80daa9a64b5901e39
KvinneKraft <KvinneKraft@protonmail.com>
2020-05-25 06:57:45 +0200

    -Code

--- Java/1.13-1.15.2/FakeEvents/src/dash/recoded/Maineroid.java
+++ Java/1.13-1.15.2/FakeEvents/src/dash/recoded/Maineroid.java
@@ -1,14 +1,143 @@
+// Author: Dashie
+// Version: 1.0
+
 package dash.recoded;
 
 public class Maineroid extends JavaPlugin implements CommandExecutor
 {
+    void LoadConfiguration()
+    {
+        saveDefaultConfig();
+
+        plugin.reloadConfig();
+        config = (FileConfiguration) plugin.getConfig();
+
+        messages.add(color(config.getString("messages.ban")));
+        messages.add(color(config.getString("messages.ipban")));
+        messages.add(color(config.getString("messages.tempban")));
+        messages.add(color(config.getString("messages.kick")));
+        messages.add(color(config.getString("messages.mute")));
+        messages.add(color(config.getString("messages.op")));
+        messages.add(color(config.getString("messages.deop")));
+        messages.add(color(config.getString("messages.vote")));
+        messages.add(color(config.getString("messages.donation")));
+
+        command_permission = config.getString("permissions.normal");
+        admin_permission = config.getString("permissions.admin");
+    };
+
+    /*final*/FileConfiguration config = (FileConfiguration) null;
+    /*final*/JavaPlugin plugin = (JavaPlugin) this;
+
     @Override public void onEnable()
     {
         print("I am getting up ....");
 
-        
+        LoadConfiguration();
+
+        getCommand("f").setExecutor(this);
 
         print("I am now alive!");
+    };
+
+    final List<String> messages = new ArrayList<>();
+    final List<String> commands = Arrays.asList
+    (
+        new String[]
+        {
+            "ban", "ipban", "tempban", "kick", "mute", "op", "deop", "vote", "donation"
+        }
+    );
+
+    String admin_permission, command_permission;
+
+    @Override public boolean onCommand(final CommandSender s, final Command c, final String a, final String[] as)
+    {
+        if (!(s instanceof Player))
+        {
+            print("I am unable to do this as a console, I must be executed as a humanoid player!");
+            return false;
+        };
+
+        final Player p = (Player) s;
+
+        if (as.length >= 1)
+        {
+            final arg = as[0].ToLowerCase();
+
+            if (p.hasPermission(admin_permission))
+            {
+                if (arg.equals("reload"))
+                {
+                    p.sendMessage(color("&eI am working on it ...."));
+
+                    LoadConfiguration();
+
+                    p.sendMessage(color("&eGuess what, I already finished!"));
+                }
+
+                else
+                {
+                    p.sendMessage(color("&cDid you mean to type &4/f reload &c?"));
+                };
+
+                return true;
+            }
+
+            else if (p.hasPermission(command_permission))
+            {
+                if (arg.equals("modules"))
+                {
+                    p.sendMessage(color("&aAvailable Modules: &a&oban [player] [reason], ipban [player] [reason], tempban [player] [reason] [time], kick [player] [reason], mute [player] [reason], op [player], deop [player], vote [player] &aand &a&odonation [player] [money]&a!"));
+                }
+
+                else if (commands.contains(arg))
+                {
+                    if (as.length < 2)
+                    {
+                        p.sendMessage(color("&cHey, you must specify a player."));
+                        return true;
+                    };
+
+                    final String message = messages.get(commands.indexOf(arg)).replace("%player%", as[1]);
+
+                    if (arg.equals("mute") || arg.equals("tempban"))
+                    {
+                        if (as.length < 4)
+                        {
+                            p.sendMessage(color("&cYou forgot to add a reason and or time."));
+                            return true;
+                        };
+
+                        message = message.replace("%duration%", as[3]).replace("%reason%", as[2]);
+                    }
+
+                    else if (arg.equals("ban") || arg.equals("ipban") || arg.equals("kick"))
+                    {
+                        if (as.length < 3)
+                        {
+                            p.sendMessage(color("&cYou forgot to add a reason."));
+                            return true;
+                        };
+
+                        message = message.replace("%reason%", as[2]);
+                    };
+
+                    getServer().broadcastMessage(color(message));
+                }
+
+                else
+                {
+                    p.sendMessage(color("&cPerhaps check the valid modules using &4/f modules &c?"));
+                };
+
+                return true;
+            };
+        };
+
+        p.sendMessage(color("&cTry adding an argument or are you not supposed to do this?"));
+        
+        return true;
     };
 
     @Override public void onDisable()
@@ -16,13 +145,12 @@ public class Maineroid extends JavaPlugin implements CommandExecutor
         print("I am no longer alive!");
     };
 
-
-    private String color(final String line)
+    String color(final String line)
     {
         return ChatColor.translateAlternateColorCodes('&', line);
     };
 
-    private void print(final String line)
+    void print(final String line)
     {
         System.out.println("(Fake Events): " + line);
     };
