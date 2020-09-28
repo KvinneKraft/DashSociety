c3a78d445528a4feb10e375a85f720a7f273af42
KvinneKraft <KvinneKraft@protonmail.com>
2020-09-29 01:37:00 +0300

    -Code

--- Public/C/DASHWARE/SimpleFlood/src/SimpleFlood.cs
+++ Public/C/DASHWARE/SimpleFlood/src/SimpleFlood.cs
@@ -25,16 +25,17 @@ namespace SimpleFlood
 {
     public partial class SimpleFlood : Form
     {
+	//<BLOCK id="application-core-section" rule="UNORDERED_COMMENT_SECTION">
 	private readonly PictureBox _logo = new PictureBox();
 	private readonly PictureBox _bar = new PictureBox();
 
 	private readonly Label _title = new Label();
-
 	public SimpleFlood()
 	{
+	    //<BLOCK id="application-initialization">
 	    try
 	    {
-		Size size = new Size(350, 235);
+		Size size = new Size(500, 400);
 
 		MinimumSize = size;
 		MaximumSize = size;
@@ -45,24 +46,43 @@ namespace SimpleFlood
 		Icon = Properties.Resources.icon;
 		Text = "Simple Flood";
 
-		BackColor = Color.FromArgb(12, 12, 12);
+		BackColor = Color.FromArgb(16, 16, 16);
 
 		_title.Left = 32;
 
 		Add.PictureBox(_bar, _logo, Properties.Resources.logo, new Size(28, 45), new Point(2, 2), Color.Transparent);
-		Add.PictureBox(this, new PictureBox(), Properties.Resources.logo, new Size(28, 45), new Point(2, 2), Color.Transparent);
+		Add.PictureBox(this, new PictureBox(), Properties.Resources.logo, new Size(28, 45), new Point(2, 2), _bar.BackColor);
 	    }
 
-	    catch (Exception e)
+	    catch 
 	    {
-		// Error Handling
+		// ERROR HANDLER
 	    };
+	    //</BLOCK>
 
 	    InterfaceSetup();
 	}
+	//</BLOCK>
+	//<BLOCK id="interface-creation-section">
+	//<BLOCK id="global-controls">
+	readonly PictureBox SPECIFICATION_AREA = new PictureBox();
+	readonly PictureBox EXECUTION_AREA = new PictureBox();
+	//</BLOCK>
+	//<BLOCK id="specification-controls">
 
+	//</BLOCK>
+	//<BLOCK id="tools-controls">
+
+	//</BLOCK>
+	//<BLOCK id="launch-controls">
+	readonly TextBox LAUNCH_LOGGER = new TextBox();
+	//</BLOCK>
 	private void InterfaceSetup()
 	{
+	    //<BLOCK id="global-area">
+	    Add.PictureBox(this, SPECIFICATION_AREA, null, new Size(Width / 2 - (2 + 25), Height - 3), new Point(2, 1), _bar.BackColor);
+	    Add.PictureBox(this, EXECUTION_AREA, null, new Size(Width - (SPECIFICATION_AREA.Width + 2), Height - 3), new Point(SPECIFICATION_AREA.Left + SPECIFICATION_AREA.Width - 2, 1), Color.FromArgb(32, 32, 32));
+	    //</BLOCK>
 	    //<BLOCK id="specification-area">
 	    try
 	    {
@@ -87,7 +107,10 @@ namespace SimpleFlood
 	    //<BLOCK id="launch-area">
 	    try
 	    {
+		Add.InputBox(EXECUTION_AREA, LAUNCH_LOGGER, EXECUTION_AREA.Size, new Point(0, 0), "sdogpfdjkgjkg", 10, 1, Color.FromArgb(32, 32, 32), Color.White, _readOnly:true);
 
+		LAUNCH_LOGGER.ScrollBars = ScrollBars.Vertical;
+		LAUNCH_LOGGER.Multiline = true;
 	    }
 
 	    catch
