eab4d795c40ee93dab76c490217088c5df6c986c
KvinneKraft <KvinneKraft@protonmail.com>
2020-09-30 12:56:17 +0300

    -code

--- Public/C/DASHWARE/SimpleFlood/src/SimpleFlood.cs
+++ Public/C/DASHWARE/SimpleFlood/src/SimpleFlood.cs
@@ -35,7 +35,7 @@ namespace SimpleFlood
 	    //<BLOCK id="application-initialization">
 	    try
 	    {
-		Size size = new Size(500, 400);
+		Size size = new Size(550, 400);
 
 		MinimumSize = size;
 		MaximumSize = size;
@@ -66,10 +66,25 @@ namespace SimpleFlood
 	//<BLOCK id="interface-creation-section">
 	//<BLOCK id="global-controls">
 	readonly PictureBox SPECIFICATION_AREA = new PictureBox();
-	readonly PictureBox EXECUTION_AREA = new PictureBox();
+
+	private void ResetLaunchLogger() => LAUNCH_LOGGER.Text = new string[] 
+	{ 
+	    "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\r\n" + 
+	    "Hey there, this tool was made for personal use and educational purposes only.\r\n\r\n" +
+
+	    "Any in proper use of this application is considered to be your responsibility and thus makes me unable to be held responsible for such.\r\n\r\n" +
+	    
+	    "Remember that things are not more to us than that what we think of such. -Dashie\r\n" +
+	    "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\r\n" +
+	    "[Status]: Waiting ....\r\n"
+	} [0];
 	//</BLOCK>
 	//<BLOCK id="specification-controls">
-
+	readonly PictureBox TARGET_AREA = new PictureBox();
+	readonly TextBox HOST = new TextBox();
+	readonly TextBox PORT = new TextBox();
+	readonly Label HOST_TEXT = new Label();
+	readonly Label PORT_TEXT = new Label();
 	//</BLOCK>
 	//<BLOCK id="tools-controls">
 
@@ -81,12 +96,26 @@ namespace SimpleFlood
 	{
 	    //<BLOCK id="global-area">
 	    Add.PictureBox(this, SPECIFICATION_AREA, null, new Size(Width / 2 - (2 + 25), Height - 3), new Point(2, 1), _bar.BackColor);
-	    Add.PictureBox(this, EXECUTION_AREA, null, new Size(Width - (SPECIFICATION_AREA.Width + 2), Height - 3), new Point(SPECIFICATION_AREA.Left + SPECIFICATION_AREA.Width - 2, 1), Color.FromArgb(32, 32, 32));
 	    //</BLOCK>
 	    //<BLOCK id="specification-area">
 	    try
 	    {
+		Add.PictureBox(SPECIFICATION_AREA, TARGET_AREA, null, new Size(SPECIFICATION_AREA.Width - 4, 100/*Update this*/), new Point(0, _bar.Top + _bar.Height + 20), _bar.BackColor);
 
+		Add.Label(TARGET_AREA, HOST_TEXT, "Host", 14, 1, Size.Empty, new Point(0, 0), Color.Transparent, Color.White);
+
+		Add.InputBox(TARGET_AREA, HOST, new Size(132, 25), new Point(HOST_TEXT.Left + HOST_TEXT.Width, HOST_TEXT.Top), "255.255.255.255", 12, 1, Color.FromArgb(56, 44, 69), Color.White, _centerText:true); HOST.TextAlign = HorizontalAlignment.Center; HOST.MaxLength = "255.255.255.255".Length;
+		Add.InputBox(TARGET_AREA, PORT, new Size(60, 25), new Point(HOST.Left + HOST.Width + 60, HOST.Top - 5), "25555", 12, 1, HOST.BackColor, HOST.ForeColor, _centerText:true);
+
+		Mod.Border(TARGET_AREA.Controls[TARGET_AREA.Controls.Count - 2], 6);
+		Mod.Border(TARGET_AREA.Controls[TARGET_AREA.Controls.Count - 1], 6);
+
+		SPECIFICATION_AREA.Paint += (s, e) =>
+		{
+		    Mod.Rectangle(e, Color.FromArgb(32, 32, 32), 1, new Size(165, 1), new Point((TARGET_AREA.Width - 165) / 2, TARGET_AREA.Top + TARGET_AREA.Height + 10));
+		};
+
+		Mod.Resize(TARGET_AREA, new Size(TARGET_AREA.Width, HOST.Top + HOST.Height + 10));
 	    }
 
 	    catch
@@ -107,10 +136,14 @@ namespace SimpleFlood
 	    //<BLOCK id="launch-area">
 	    try
 	    {
-		Add.InputBox(EXECUTION_AREA, LAUNCH_LOGGER, EXECUTION_AREA.Size, new Point(0, 0), "sdogpfdjkgjkg", 10, 1, Color.FromArgb(32, 32, 32), Color.White, _readOnly:true);
+		Add.InputBox(this, LAUNCH_LOGGER, new Size(Width - (SPECIFICATION_AREA.Width + 2), Height - (_bar.Height + 3)), new Point(SPECIFICATION_AREA.Left + SPECIFICATION_AREA.Width - 2, _bar.Height + 1), "sdogpfdjkgjkg", 10, 1, Color.FromArgb(32, 32, 32), Color.White, _readOnly:true, _centerText:false, _border:false, Color.Empty);
 
 		LAUNCH_LOGGER.ScrollBars = ScrollBars.Vertical;
 		LAUNCH_LOGGER.Multiline = true;
+
+		ResetLaunchLogger();
+
+		Add.PictureBox(SPECIFICATION_AREA, new PictureBox(), null, new Size(3, Height), new Point(LAUNCH_LOGGER.Left - 3, 0), LAUNCH_LOGGER.BackColor);
 	    }
 
 	    catch
