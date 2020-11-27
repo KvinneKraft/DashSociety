64ff7bbd75fbd990ee5180c96d28189f16bec17d
KvinneKraft <KvinneKraft@protonmail.com>
2020-11-28 00:36:49 +0200

    -Code

--- Public/C/DASHWARE/Unreleased/ThaDasher/Interface.cs
+++ Public/C/DASHWARE/Unreleased/ThaDasher/Interface.cs
@@ -8,10 +8,10 @@ namespace ThaDasher
 {
     public partial class Interface : Form
     {
-	private readonly static string FORMAT = "Hey there, I am unfortunate to tell you but an error has occurred which has caused this application to be useless right now.\r\n\r\nYou can help me prevent this from happening in the future by sending me the following error code at KvinneKraft@protonmail.com:\r\n{\r\n%m%\r\n}\r\n\r\nRegardless, Thank you.\r\n\r\nClick OK to close this application or click cancel to restart this application.";
+	readonly static string FORMAT = "Hey there, I am unfortunate to tell you but an error has occurred which has caused this application to be useless right now.\r\n\r\nYou can help me prevent this from happening in the future by sending me the following error code at KvinneKraft@protonmail.com:\r\n{\r\n%m%\r\n}\r\n\r\nRegardless, Thank you.\r\n\r\nClick OK to close this application or click cancel to restart this application.";
 
-	private readonly static DashControls CONTROL = new DashControls();
-	private readonly static DashTools TOOL = new DashTools();
+	readonly static DashControls CONTROL = new DashControls();
+	readonly static DashTools TOOL = new DashTools();
 
 	private void InitializeMBar()
 	{
@@ -49,9 +49,9 @@ namespace ThaDasher
 	    readonly public static TextBox PO_BOX = new TextBox() { Text = "8080", TextAlign = HorizontalAlignment.Center };
 	    readonly public static TextBox DR_BOX = new TextBox() { Text = "360", TextAlign = HorizontalAlignment.Center };
 
-	    readonly public static Label IP_LAB = new Label();
-	    readonly public static Label PO_LAB = new Label();
-	    readonly public static Label DR_LAB = new Label();
+	    readonly static Label IP_LAB = new Label();
+	    readonly static Label PO_LAB = new Label();
+	    readonly static Label DR_LAB = new Label();
 
 	    public static void InitializeHCon(Form TOP)
 	    {
@@ -137,39 +137,7 @@ namespace ThaDasher
 
 	private class SETTINGSCONTAINER
 	{
-	    readonly public static Label HTTP_TITLE = new Label() { Text = "HTTP Methods", TextAlign = ContentAlignment.MiddleCenter };
-	    
-	    readonly public static Button HTTP_DASHLORIS = new Button();
-	    readonly public static Button HTTP_SLOWLORIS = new Button();
-	    readonly public static Button HTTP_FLOOD = new Button();
-	    readonly public static Button HTTP_POST = new Button();
-	    readonly public static Button HTTP_PUT = new Button();
-	    readonly public static Button HTTP_GET = new Button();
-
-	    readonly public static int httpcount = 6;
-
-	    readonly public static Label TCP_TITLE = new Label() { Text = "TCP Methods", TextAlign = ContentAlignment.MiddleCenter };
-	    
-	    readonly public static Button TCP_LONGSOCKS = new Button();
-	    readonly public static Button TCP_SOCKS = new Button();
-	    readonly public static Button TCP_FLOOD = new Button();
-	    readonly public static Button TCP_WAVES = new Button();
-	    
-	    readonly public static int tcpcount = 4;
-
-	    readonly public static Label UDP_TITLE = new Label() { Text = "UDP Methods", TextAlign = ContentAlignment.MiddleCenter };
-	    
-	    readonly public static Button UDP_OVERLOAD = new Button();
-	    readonly public static Button UDP_FLOOD = new Button();
-	    readonly public static Button UDP_WAVES = new Button();
-	    readonly public static Button UDP_HAM = new Button();
-	    
-	    readonly public static int udpcount = 4;
-
-	    readonly public static PictureBox BAR = new PictureBox();
-	    readonly public static Button CLOSE = new Button();
-	    readonly public static Label TITLE = new Label();
-	    readonly public static Form DIALOG = new Form();
+	    readonly static Form DIALOG = new Form();
 
 	    private static void InitializeDialogy()
 	    {
@@ -197,6 +165,12 @@ namespace ThaDasher
 		    throw new Exception("Selector Container Initialization.");
 		}
 	    }
+
+	    readonly static PictureBox BAR = new PictureBox();
+
+	    readonly static Button CLOSE = new Button();
+
+	    readonly static Label TITLE = new Label();
 
 	    private static void InitializeMenuBar()
 	    {
@@ -233,6 +207,33 @@ namespace ThaDasher
 		    throw new Exception("Selector Container Menu Bar Initialization.");
 		}
 	    }
+
+	    readonly static Label HTTP_TITLE = new Label() { Text = "HTTP Methods", TextAlign = ContentAlignment.MiddleCenter };
+
+	    readonly static Button HTTP_DASHLORIS = new Button();
+	    readonly static Button HTTP_SLOWLORIS = new Button();
+	    readonly static Button HTTP_FLOOD = new Button();
+	    readonly static Button HTTP_POST = new Button();
+	    readonly static Button HTTP_PUT = new Button();
+	    readonly static Button HTTP_GET = new Button();
+
+	    readonly static int httpcount = 6;
+
+	    readonly static Label TCP_TITLE = new Label() { Text = "TCP Methods", TextAlign = ContentAlignment.MiddleCenter };
+
+	    readonly static Button TCP_LONGSOCKS = new Button();
+	    readonly static Button TCP_SOCKS = new Button();
+	    readonly static Button TCP_FLOOD = new Button();
+	    readonly static Button TCP_WAVES = new Button();
+
+	    readonly static int tcpcount = 4;
+
+	    readonly static Label UDP_TITLE = new Label() { Text = "UDP Methods", TextAlign = ContentAlignment.MiddleCenter };
+
+	    readonly static Button UDP_OVERLOAD = new Button();
+	    readonly static Button UDP_FLOOD = new Button();
+	    readonly static Button UDP_WAVES = new Button();
+	    readonly static Button UDP_HAM = new Button();
 
 	    private static void InitializeOptions()
 	    {
@@ -307,20 +308,20 @@ namespace ThaDasher
 	    }
 
 	    readonly public static PictureBox CONTAINER = new PictureBox();
-	    readonly public static PictureBox PONY1 = new PictureBox();
-	    readonly public static PictureBox PONY2 = new PictureBox();
+	    readonly static PictureBox PONY1 = new PictureBox();
+	    readonly static PictureBox PONY2 = new PictureBox();
 
 	    readonly public static Button METHODS = new Button();
 
-	    readonly public static Label METHOD_TITLE = new Label() { TextAlign = ContentAlignment.MiddleCenter };
+	    readonly static Label METHOD_TITLE = new Label() { TextAlign = ContentAlignment.MiddleCenter };
 
 	    public static void InitializeMCon(Form TOP)
 	    {
 		try
 		{
-		    var CONTAINER_SIZE = new Size(CONTAINER.Width, 100);
-		    var CONTAINER_LOCA = new Point(CONTAINER.Left, CONTAINER.Top + CONTAINER.Height + 5);
-		    var CONTAINER_BCOL = CONTAINER.BackColor;
+		    var CONTAINER_SIZE = new Size(TARGETCONTAINER.CONTAINER.Width, 100);
+		    var CONTAINER_LOCA = new Point(TARGETCONTAINER.CONTAINER.Left, TARGETCONTAINER.CONTAINER.Top + TARGETCONTAINER.CONTAINER.Height + 5);
+		    var CONTAINER_BCOL = TARGETCONTAINER.CONTAINER.BackColor;
 
 		    CONTROL.Image(TOP, CONTAINER, CONTAINER_SIZE, CONTAINER_LOCA, null, CONTAINER_BCOL);
 
@@ -329,7 +330,6 @@ namespace ThaDasher
 		    var RECTANGLE_COLA = Color.FromArgb(CONTAINER_BCOL.R - 8, CONTAINER_BCOL.G - 8, CONTAINER_BCOL.B - 8);
 
 		    TOOL.PaintRectangle(CONTAINER, 4, RECTANGLE_SIZE, RECTANGLE_LOCA, RECTANGLE_COLA);
-
 		    TOOL.Round(CONTAINER, 6);
 
 		    var TITLE_SIZE = Size.Empty;
@@ -347,7 +347,6 @@ namespace ThaDasher
 		    var METHOD_FCOL = Color.White;
 
 		    CONTROL.Button(CONTAINER, METHODS, METHOD_SIZE, METHOD_LOCA, METHOD_BCOL, METHOD_FCOL, 1, 10, "HTTP -- Dashloris 4.0", Color.Empty);
-
 		    TOOL.Round(METHODS, 4);
 
 		    METHODS.Click += (s, e) => DIALOG.ShowDialog();
@@ -378,11 +377,10 @@ namespace ThaDasher
 
 	private class LOGCONTAINER
 	{
+	    readonly public static RichTextBox LOG = new RichTextBox();
 	    readonly public static PictureBox CONTAINER = new PictureBox();
 
-	    readonly public static RichTextBox LOG = new RichTextBox();
-
-	    readonly public static Button CLEAR = new Button();
+	    readonly static Button CLEAR = new Button();
 
 	    public static void SetupLogContainer()
 	    {
@@ -393,9 +391,9 @@ namespace ThaDasher
 	    {
 		try
 		{
-		    var CONTAINER_SIZE = new Size(TOP.Width - 2 - CONTAINER.Left - 14 - CONTAINER.Width, CONTAINER.Height + 5 + SETTINGSCONTAINER.CONTAINER.Height);
-		    var CONTAINER_LOCA = new Point(TOP.Width - 1 - CONTAINER_SIZE.Width - 8, CONTAINER.Top);
-		    var CONTAINER_COLA = CONTAINER.BackColor;
+		    var CONTAINER_SIZE = new Size(TOP.Width - 2 - TARGETCONTAINER.CONTAINER.Left - 14 - TARGETCONTAINER.CONTAINER.Width, TARGETCONTAINER.CONTAINER.Height + 5 + SETTINGSCONTAINER.CONTAINER.Height);
+		    var CONTAINER_LOCA = new Point(TOP.Width - 1 - CONTAINER_SIZE.Width - 8, TARGETCONTAINER.CONTAINER.Top);
+		    var CONTAINER_COLA = TARGETCONTAINER.CONTAINER.BackColor;
 
 		    CONTROL.Image(TOP, CONTAINER, CONTAINER_SIZE, CONTAINER_LOCA, null, CONTAINER_COLA);
 
@@ -452,9 +450,9 @@ namespace ThaDasher
 	    readonly public static PictureBox BUTTON_CONTAINER = new PictureBox();
 	    readonly public static PictureBox CONTAINER = new PictureBox();
 
-	    readonly public static Button ABOUT = new Button();
-	    readonly public static Button START = new Button();
-	    readonly public static Button PORTS = new Button();
+	    readonly static Button ABOUT = new Button();
+	    readonly static Button START = new Button();
+	    readonly static Button PORTS = new Button();
 
 	    public static void InitializeTCon(Form TOP)
 	    {
@@ -497,10 +495,25 @@ namespace ThaDasher
 
 	private class LOGINDIALOG
 	{
-	    public static void UndoChanges(Form TOP)
+	    readonly static PictureBox BAR_CONTAINER = new PictureBox();
+
+	    readonly static Button CLOSE = new Button();
+	    readonly static Button MINIM = new Button();
+
+	    readonly static Label TITLE = new Label();
+
+	    private static void InitializeMEU(Form TOP)
 	    {
+
+	    }
+
+	    public static void UndoChanges(Form TOP)
+	    {//Re-Do this<----
 		foreach (Control Control in TOP.Controls)
 		{
+		    Control.Visible = false;
+
+		    Control.Hide();
 		    Control.Dispose();
 		}
 
@@ -512,6 +525,8 @@ namespace ThaDasher
 		TOOL.Resize(TOP, new Size(225, 125));
 		TOOL.Round(TOP, 6);
 
+		TOP.FormBorderStyle = FormBorderStyle.None;
+		TOP.StartPosition = FormStartPosition.CenterScreen;
 		TOP.BackColor = Color.FromArgb(18, 18, 18);
 
 		var RECTANGLE_SIZE = new Size(TOP.Width - 1, TOP.Height - 1);
@@ -521,32 +536,24 @@ namespace ThaDasher
 		TOOL.PaintRectangle(TOP, 2, RECTANGLE_SIZE, RECTANGLE_LOCA, RECTANGLE_COLA);
 	    }
 
-	    readonly public static TextBox IUSERNAME = new TextBox();
-	    readonly public static TextBox IPASSWORD = new TextBox();
+	    readonly static TextBox IUSERNAME = new TextBox();
+	    readonly static TextBox IPASSWORD = new TextBox();
 
-	    readonly public static Label USERNAME = new Label();
-	    readonly public static Label PASSWORD = new Label();
+	    readonly static Label USERNAME = new Label();
+	    readonly static Label PASSWORD = new Label();
 
 	    private static void InitializeCRE(Form TOP)
 	    {
 		Size GetPreferredSize(string TEXT, int SIZE) =>
 		    TextRenderer.MeasureText(TEXT, TOOL.GetFont(1, SIZE));
 
-		Control GetAnonControl() =>
-		    TOP.Controls[TOP.Controls.Count - 1];
-
 		var LABEL_SIZE = GetPreferredSize("Username:", 12);
-
-		Int32 GetLabelWidth() =>
-		    TOP.Width - LABEL_SIZE.Width - 15;
-
 		var LABEL_LOCA = new Point(5, 31);
-
-		Int32 GetLabelX() =>
-		    LABEL_SIZE.Width + LABEL_LOCA.X;
-
 		var LABEL_BCOL = TOP.BackColor;
 		var LABEL_FCOL = Color.White;
+
+		int GetLabelWidth() => TOP.Width - LABEL_SIZE.Width - 15;
+		int GetLabelX() => LABEL_SIZE.Width + LABEL_LOCA.X;
 
 		CONTROL.Label(TOP, USERNAME, LABEL_SIZE, LABEL_LOCA, LABEL_BCOL, LABEL_FCOL, 1, 12, "Username:");
 
@@ -555,7 +562,10 @@ namespace ThaDasher
 		var BOX_BCOL = Color.MidnightBlue;
 		var BOX_FCOL = Color.White;
 
+		Control GetAnonControl()=>TOP.Controls[TOP.Controls.Count - 1];
+
 		CONTROL.TextBox(TOP, IUSERNAME, BOX_SIZE, BOX_LOCA, BOX_BCOL, BOX_FCOL, 1, 10, Color.Empty);
+
 		TOOL.Round(GetAnonControl(), 6);
 
 		LABEL_SIZE = GetPreferredSize("Password:", 12);
@@ -567,13 +577,14 @@ namespace ThaDasher
 		BOX_LOCA = new Point(GetLabelX(), LABEL_LOCA.Y + 1);
 
 		CONTROL.TextBox(TOP, IPASSWORD, BOX_SIZE, BOX_LOCA, BOX_BCOL, BOX_FCOL, 1, 10, Color.Empty);
+
 		TOOL.Round(GetAnonControl(), 6);
 	    }
 
-	    readonly public static PictureBox CONTAINER = new PictureBox();
+	    readonly static PictureBox BUTTON_CONTAINER = new PictureBox();
 
 	    readonly public static Button LOGIN = new Button();
-	    readonly public static Button HELP = new Button();
+	    readonly static Button HELP = new Button();
 
 	    private static void InitializeAUT(Form TOP)
 	    {
@@ -581,24 +592,25 @@ namespace ThaDasher
 		var CONTAINER_LOCA = new Point((TOP.Width - CONTAINER_SIZE.Width) / 2, TOP.Height - CONTAINER_SIZE.Height - 7);
 		var CONTAINER_COLA = TOP.BackColor;
 
-		CONTROL.Image(TOP, CONTAINER, CONTAINER_SIZE, CONTAINER_LOCA, null, CONTAINER_COLA);
+		CONTROL.Image(TOP, BUTTON_CONTAINER, CONTAINER_SIZE, CONTAINER_LOCA, null, CONTAINER_COLA);
 
 		var BUTTON_SIZE = new Size(CONTAINER_SIZE.Width / 2 - 5, CONTAINER_SIZE.Height);
 		var BUTTON_LOCA = new Point(0, 0);
 		var BUTTON_BCOL = Color.FromArgb(12, 12, 12);
 		var BUTTON_FCOL = Color.White;
 
-		CONTROL.Button(CONTAINER, LOGIN, BUTTON_SIZE, BUTTON_LOCA, BUTTON_BCOL, BUTTON_FCOL, 1, 11, "Login", Color.Empty);
+		CONTROL.Button(BUTTON_CONTAINER, LOGIN, BUTTON_SIZE, BUTTON_LOCA, BUTTON_BCOL, BUTTON_FCOL, 1, 11, "Login", Color.Empty);
 		TOOL.Round(LOGIN, 6);
 
 		BUTTON_LOCA.X += BUTTON_SIZE.Width + 10;
 
-		CONTROL.Button(CONTAINER, HELP, BUTTON_SIZE, BUTTON_LOCA, BUTTON_BCOL, BUTTON_FCOL, 1, 11, "Help", Color.Empty);
+		CONTROL.Button(BUTTON_CONTAINER, HELP, BUTTON_SIZE, BUTTON_LOCA, BUTTON_BCOL, BUTTON_FCOL, 1, 11, "Help", Color.Empty);
 		TOOL.Round(HELP, 6);
 	    }
 
 	    public static void AuthenticatorInterf(Form TOP)
 	    {
+		InitializeMEU(TOP);
 		InitializeGUI(TOP);
 		InitializeCRE(TOP);
 		InitializeAUT(TOP);
@@ -607,19 +619,17 @@ namespace ThaDasher
 
 	public Interface()
 	{
-
 	    try
 	    {
-		InitializeComponent();
-
 		LOGINDIALOG.AuthenticatorInterf(this);
-
+		
 		LOGINDIALOG.LOGIN.Click += (s, e) =>
 		{
 		    // Do-Login-Work
 
 		    LOGINDIALOG.UndoChanges(this);
 
+		    InitializeComponent();
 		    InitializeMBar();
 		    InitializeBody();
 
