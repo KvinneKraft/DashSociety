acdd338603d8d2842d26828640ff192279674bdc
KvinneKraft <KvinneKraft@protonmail.com>
2020-12-26 00:54:55 +0200

    -Code

---
+++ java/latest-2021/NoPickupX/src/main/java/com/kvinnekraft/NoPickupX.java
@@ -0,0 +1,16 @@
+package com.kvinnekraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NoPickupX extends JavaPlugin
+{
+    @Override public final void onEnable()
+    {
+
+    }
+
+    @Override public final void onDisable()
+    {
+
+    }
+}
